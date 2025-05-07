f3197aa7765ae8b1de4c96549d688a1433fbdea7
Miguel <ventas@aurorasistemas.com>
2025-05-07 13:01:57 -0400

    feat: Implementación de conversión automática de fechas a UTC en ApplicationDbContext

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,5 +1,9 @@
 using Microsoft.EntityFrameworkCore;
 using SistemaContable.Models;
+using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace SistemaContable.Data
 {
@@ -20,6 +24,50 @@ namespace SistemaContable.Data
             base.OnModelCreating(modelBuilder);
 
             modelBuilder.Entity<Empresa>().ToTable("Empresas");
+        }
+
+        public override int SaveChanges()
+        {
+            ConvertDatesToUtc();
+            return base.SaveChanges();
+        }
+
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            ConvertDatesToUtc();
+            return base.SaveChangesAsync(cancellationToken);
+        }
+
+        private void ConvertDatesToUtc()
+        {
+            var entities = ChangeTracker.Entries()
+                .Where(e => e.State == EntityState.Added || e.State == EntityState.Modified);
+
+            foreach (var entity in entities)
+            {
+                var dateProperties = entity.Entity.GetType().GetProperties()
+                    .Where(p => p.PropertyType == typeof(DateTime) || p.PropertyType == typeof(DateTime?));
+
+                foreach (var property in dateProperties)
+                {
+                    if (property.PropertyType == typeof(DateTime))
+                    {
+                        var value = (DateTime)property.GetValue(entity.Entity);
+                        if (value.Kind != DateTimeKind.Utc)
+                        {
+                            property.SetValue(entity.Entity, DateTime.SpecifyKind(value, DateTimeKind.Utc));
+                        }
+                    }
+                    else if (property.PropertyType == typeof(DateTime?))
+                    {
+                        var value = (DateTime?)property.GetValue(entity.Entity);
+                        if (value.HasValue && value.Value.Kind != DateTimeKind.Utc)
+                        {
+                            property.SetValue(entity.Entity, DateTime.SpecifyKind(value.Value, DateTimeKind.Utc));
+                        }
+                    }
+                }
+            }
         }
     }
 } 
