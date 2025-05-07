5664977f956cb2e9deaeeadc26a643c2c17af3b4
Miguel <ventas@aurorasistemas.com>
2025-05-07 19:31:10 -0400

    DB: Migración para eliminar restricciones requeridas en cuentas bancarias

---
+++ Migrations/20250507230503_RemoveBankAccountRequiredConstraint.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SistemaContable.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveBankAccountRequiredConstraint : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
